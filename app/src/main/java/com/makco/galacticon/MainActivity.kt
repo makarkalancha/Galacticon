0558ba0507e34726e2753b033d53d4b87d04edf5
Makar Kalancha <makarkalancha>
2024-03-08 16:39:10 -0500

    binding

--- app/src/main/java/com/makco/galacticon/MainActivity.kt
+++ app/src/main/java/com/makco/galacticon/MainActivity.kt
@@ -2,10 +2,23 @@ package com.makco.galacticon
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.makco.galacticon.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        linearLayoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = linearLayoutManager
     }
 }
